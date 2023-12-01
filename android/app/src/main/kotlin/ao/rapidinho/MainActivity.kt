2b29fcbf49cd4b42b0759f1d1c43cbb42063c819
KippesMarcos <kippesmarcos11@gmail.com>
2023-11-30 21:06:01 -0300

    import io.flutter.embedding.android.FlutterActivity;

--- android/app/src/main/kotlin/ao/rapidinho/MainActivity.kt
+++ android/app/src/main/kotlin/ao/rapidinho/MainActivity.kt
@@ -2,7 +2,7 @@ package ao.rapidinho
 
 import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity(): FlutterActivity() {
